0c416a68d49275dc9727cb69e3f001a57cc9d967
j.takahashi <j.takahashi@stageinc.jp>
2021-08-21 20:00:43 +0900

    implement callbacks with useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = (props) => {
   const [state, setState] = useState(props)
   const {name, price} = state
+
+  // useEffectの特徴
+  // ・レンダリングの直後に毎回呼ばれる
+  // ・第二引数に空の配列[]を渡すと最初の1回のみ呼ばれる
+  // ・コンポーネント内に複数記述可能
+  // ・特定の要素の状態が変更になった場合だけ実行させる指定可能
+
+  // 初回のみ実行
+  useEffect(() => {
+    console.log('useEffect初回のみ')
+  }, [])
+  // レンダリング時実行
+  useEffect(() => {
+    console.log('useEffectレンダリング毎')
+  })
+  // 初回と特定の要素の状態が変更された場合のみ実行
+  useEffect(() => {
+    console.log('useEffect_name状態変更時のみ')
+  }, [name])
 
   return (
     <>
