2043b5e2c1568a129b2356ced72d08246c05669c
j.takahashi <j.takahashi@stageinc.jp>
2021-08-28 22:20:43 +0900

    replace type names with constants

---
+++ src/actions/index.js
@@ -0,0 +1,6 @@
+// 定数のエクスポート
+export const CREATE_EVENT = 'CREATE_EVENT'
+export const DELETE_EVENT = 'DELETE_EVENT'
+export const DELETE_ALL_EVENT = 'DELETE_ALL_EVENT'
+
+// typeでタイプミスがあるとエラーが検出されないのでこのやり方をする
