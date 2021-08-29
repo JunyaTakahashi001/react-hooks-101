28f7bec04489d1169a2165b5965cb3ac0676c52a
j.takahashi <j.takahashi@stageinc.jp>
2021-08-29 09:22:38 +0900

    setup react context provider

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 // 定数のエクスポート
 export const CREATE_EVENT = 'CREATE_EVENT'
 export const DELETE_EVENT = 'DELETE_EVENT'
-export const DELETE_ALL_EVENT = 'DELETE_ALL_EVENT'
+export const DELETE_ALL_EVENTS = 'DELETE_ALL_EVENTS'
 
 // typeでタイプミスがあるとエラーが検出されないのでこのやり方をする
